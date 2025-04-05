08e24b1ab6a131a23ab6635260d3a3a6e73710aa
MaRiSink <mashik.sinkevich@gmail.com>
2025-04-05 23:01:37 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,91 +10,139 @@ void paradise()
 
 void death_prisoner()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  So you're dead... You don't exist anymore. Stop." << endl;
+     cout << " You see a light and walk towards it, adjusting your prison uniform." << endl;
+    cout << " Turning around, you see a huge shell casing rolling towards you, " << endl;
+    cout << "which is clearly a good motivator to go faster. Then even faster." << endl;
+    cout << " And now you're running, after which you find yourself in a white corridor, " << endl;
+    cout << "after which you see something that looks like a polish uzhond. " << endl;
+    cout << " You take a ticket and have to wait..." << endl;
+    //Автокликер
+    //кликай на полоску, чтобы она загрузилась и время подошло к твоей очереди
 }
 
 void death_old_age() //domohozajka; pisatel; wrach-detektiwnyj
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  And so Jas, surrounded by loving people, left this world. " << endl;
+    cout << " You feel that your joints no longer hurt, " << endl;
+    cout << "that arthritis is as if it never happened and you are twenty years old again." << endl;
+    cout << " Two little angels meet you and lead you to the white light." << endl;
+      cout << " Without hurrying, you can look around, admire the beauty of these places." << endl;
+    cout << " After which you are led to the building of the polish uzhond." << endl;
+    cout << " Since you are a pensioner and have lived a good, correct life, you are allowed to skip the line. " << endl;
+    cout << " Now your task is to choose where to go" << endl;
+     //имитация комнаты и человечек. Пользователь нажимает вверх либо вниз.
+    //Вверх-рай
+   //Вниз-перерождение
 }
 
-void death_pretty_kitty()
+void death_pretty_kitties()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  Nothing, then fear. You see three tigers in front of you, carrying you on a throne. " << endl;
+    cout << " At first you think it's a hallucination. You're young, beautiful, and it couldn't be like this. " << endl;
+    cout << " You're brought to a snow-white building of the uzhond, where you stand and wait in line." << endl;
+    cout << " Then you hand over the documents that appeared out of nowhere and they stamp them with the word (Rebirth)" << endl;
+    //появляется окошко и пользователю нужно двигаться вниз-перерождение. Но вверх-рай. Куда он пойдет-выбор его
 }
 
 void death_arabic_sheikh()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  Having fallen asleep peacefully, you went to Allah. At least that was how it was planned." << endl;
+    cout << " But when you opened your eyes, you were being driven by an unfamiliar driver in your Lamborghini, some model. " << endl;
+    cout << " You were brought to a large building and left there, " << endl;
+    cout << "after which you were dropped off and gently asked to wait in the Polish uzhond." << endl;
+    cout << " Soon it was your turn and you were given a ticket to the sanatoriums of Allah" << endl;
+    //Комната и проход
 }
 
 void death_spaceman()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  It seemed like you were dead, but you saw that you were in a rocket." << endl;
+    cout << " It was carrying you somewhere far away, leaving the earth far behind. " << endl;
+    cout << " Unexpectedly, the landing, which should have been sharp, turned out to be gentle, like a jump up a mountain of pillows." << endl;
+    cout << " At the exit, there was only one building waiting for you. HE… Uzhond. Well." << endl;
+    cout << " You had to wait, and then it was your turn and you received a death certificate and a pass to heaven. " << endl;
+    cout << " Welcome to a world of peace and light, a world where you will continue to be a hero to many." << endl;
+    //Комната и проход
 }
 
-void death_department()
+void death_uzhond()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  So… You seem to have come to work leisurely. But what is this? " << endl;
+    cout << " Why should you sit and wait for your turn?" << endl;
+    cout << "Knowing your colleagues, you understand that it will take forever. " << endl;
+    cout << endl;
+    cout << " Tap to skip time" << endl;
+    //«Опять симулятор, где нужно нажимать на кнопочку, для загрузки времени»
+   //«После окончания появляется еще одна»
+   cout << " ETERNAL WAITING" << endl;
 }
 
 void death_librian()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  Opening your eyes, you feel like you've fallen asleep in the library. " << endl;
+    cout << " Getting up, you frame your beloved possessions at your disposal, then step inside the book corridors and go get a coffee." << endl;
+    cout << " But there's one problem... Everywhere are -one second break books-; -one second break- books and more books." << endl;
+    cout << " Finally, you reach your desk, where lies... OH MIRACLE!" << endl;
+    cout << "  Another book.  " << endl;
+    cout << " It says -Your Death.- When you open it, you were sucked inside. " << endl;
+    cout << " You find yourself in a uzhond, wait in line, and then you get it: a ticket to heaven." << endl;
+    cout << " A creaky voice says: " << endl;
+    cout << "  - Go straight ahead and enter this corridor. That's where you need to go." << endl;
+    //Появляется функция и надо зайти туда
+    cout << " Finally, silence…." << endl;
 }
 
 void death_historical()
 {
+    cout << "  Darkness..." << endl;
+    cout << " The smell of earth..." << endl;
+    cout << " Then a harsh light and..." << endl;
+    cout << " Moles?" << endl;
+    cout << " No, seriously. You were riding on a mole while other moles crawled along holding your recorder and a piece of paper." << endl;
     cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << " So, this Uber Business Class taxi finally got you somewhere..." << endl;
+    cout << " The white building of the office where you met your history group. " << endl;
+    cout << " Then the accounting department..." << endl;
+    cout << " A death certificate and directions to where you'd better go." << endl;
+    //Два прохода в комнате. Перерождение и Рай
 }
 
 void death_0_kidneys()
 {
+    cout << "  Drying….Strong…" << endl;
+    cout << " You woke up in the middle of the desert and it seemed like you had been wandering for some time. " << endl;
+    cout << " Tired and clearly irritated. Soon the withdrawal began. " << endl;
+    cout << " Strong and creating a sour taste of aggression in your mouth. " << endl;
+    cout << " You wanted to drink, but at the same time it seemed like you couldn’t sober up." << endl;
+    cout << " Here it is. A building in the middle of the desert. Uzhond. " << endl;
+    cout << " You crawled up the stairs, but then a guard stopped you, who said in a deep voice:" << endl;
+    cout << "  -You have no right to enter a government institution while drunk." << endl;
+    cout << " You tried to object, but they wouldn't let you in. Drunk, you were forced to wander the desert for eternity... " << endl;
+    cout << " Without a goal... Without alcohol, but forever drunk... Without kidneys..." << endl;
     cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "   GAME OVER  " << endl;
+    //бутылка пива
 }
 
 void death_dantist()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "  Opening your eyes, you experience mixed feelings... there is a huge set of false teeth with eyes next to you. " << endl;
+    cout << " It is not aggressive, but what the hell is going on?? " << endl;
+    cout << " Then, after a loud sound, you see a gypsy camp. " << endl;
+    cout << " You are suddenly lifted up together with this jaw and carried away somewhere with loud exclamations." << endl;
+    cout << " Eh... As Alexander Sergeevich Pushkin wrote" << endl;
+    cout << "'Gypsies roam noisily in crowds across Bessarabia" << endl;
+    cout << " Today they spend the night in tattered tents above the river'" << endl;
+    cout << "   (Translation)   " << endl;
+    cout << " But this time no one spent the night. You (unlike the false jaw) were suddenly laid down. " << endl;
+    cout << "  You realized that you were near the uzhond. Since there was nothing around, you decided to go in anyway." << endl;
+    cout << " You were pleased with the sterility as in your favorite dentistry." << endl;
+    cout << " A small queue is also a common sight. After that, you got your ticket to heaven" << endl;
+    //Пройти туда
 }
 
-void death_writer()
-{
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-}
+
 
 int main()
 {
@@ -126,11 +174,21 @@ int main()
     {
         cout << endl;
     }
-    swich (tab[0][i])
+    switch (tab[0][i])
     {
 case 0:
     SetConsoleTextAttribute(h, 42);
             cout << tab[0][i] ;
+            break;
+case 1:
+    SetConsoleTextAttribute(h, 48);
+            cout << tab[0][i];
+            break;
+            default:
+            SetConsoleTextAttribute(h, 47);
+            cout << tab[0][i];
+    }
+
         /*if (tab[0][i] == 1)
         {
            SetConsoleTextAttribute(h, 42);
@@ -145,5 +203,7 @@ case 0:
         }*/
     }
     cout<<endl;
-    SetConsoleTextAttribute(h, 0);
+   // SetConsoleTextAttribute(h, 0);
 }
+
+
